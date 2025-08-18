1c8f5d3f156a9f11a5fb0309c2ef090ce52f6dd1
Fadel Bantan <fadelbantan@Fadels-MacBook-Pro.local>
2025-08-18 17:08:06 +0300

    Moving logic

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
+import pygame
 from constants import *
 from player import Player
-import pygame
+
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    # GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-
-    # Drawing the game onto the screen
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("Black")
+
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-    clock.tick(60)
 
-    dt = clock.tick.get_time() / 1000
-    return dt
-
-    Player()
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
