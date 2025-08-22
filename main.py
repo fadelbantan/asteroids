cfe4f73152e4ac4b5f9ecd476cb40ac4693dbbd0
Fadel Bantan <fadelbantan@Fadels-MacBook-Pro.local>
2025-08-22 23:21:19 +0300

    Add shooting and asteroid splitting mechanics

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
 
-
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,18 +14,17 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-
     shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
-    drawable = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -42,7 +39,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
+
         for obj in drawable:
             obj.draw(screen)
 
